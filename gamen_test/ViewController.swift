e6369460f5ec03f272382a2a1635fe7154c74ea0
dobocreate <kishida@dobocreate.jp>
2021-08-25 23:14:36 +0900

    Initial Commit

---
+++ gamen_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gamen_test
+//
+//  Created by 岸田展明 on 2021/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
